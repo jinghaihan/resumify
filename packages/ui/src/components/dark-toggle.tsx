10521dd101dd4a5a700d7f11ebb95930e1bd8b19
jinghaihan <jhh19980114@gmail.com>
2026-02-19 23:38:33 +0800

    fix: dark toggle

--- packages/ui/src/components/dark-toggle.tsx
+++ packages/ui/src/components/dark-toggle.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useIsMounted } from '@resumify/shared'
+// import { useIsMounted } from '@resumify/shared'
 import { MoonIcon, SunIcon } from 'lucide-react'
 import { useTranslations } from 'next-intl'
 import { useTheme } from 'next-themes'
@@ -11,9 +11,9 @@ export function DarkToggle() {
   const t = useTranslations()
   const { theme, setTheme } = useTheme()
 
-  const isMounted = useIsMounted()
-  if (!isMounted())
-    return null
+  // const isMounted = useIsMounted()
+  // if (!isMounted())
+  //   return null
 
   return (
     <IconButton
